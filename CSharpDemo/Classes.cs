2cbe6bd565677da7e799c99d2f5fa7392631e35c
Li <jili1@uiwtx.edu>
2021-10-14 15:25:36 -0500

    init commit

---
+++ CSharpDemo/Classes.cs
@@ -0,0 +1,227 @@
+﻿
+namespace CSharpDemo
+{
+    class HealthComp
+    {
+        public delegate void OnDie();
+        public delegate void OnHealthChange(float newValue);
+
+        OnDie _onDie;
+        public OnDie onDie
+        {
+            get { return _onDie; }
+            set { _onDie = value; }
+        }
+
+        OnHealthChange _onHealthChange;
+        public OnHealthChange onHealthChange
+        {
+            get { return _onHealthChange; }
+            set { _onHealthChange = value; }
+        }
+
+        public HealthComp(float health)
+        {
+            this.health = health;
+        }
+        float health;
+        public float Health
+        {
+            set 
+            {
+                health = value;
+                onHealthChange.Invoke(health);
+                if(health <= 0)
+                {
+                    onDie.Invoke();
+                }
+            }
+            get { return health; }
+        }
+
+        public void TakeDamage(float damage)
+        {
+            Health -= damage;
+        }
+    }
+
+    interface IObjectGenericInfo
+    {
+        float GetWeight();
+        string GetName();
+    }
+
+    class Animal : IObjectGenericInfo
+    {
+        public float GetWeight() { return weight; }
+        public string GetName() { return name; }
+        //constructor
+        public Animal()
+        {
+        }
+
+        public Animal(string AnimalName, int AnimalAge, float AnimalWeight)
+        {
+            name = AnimalName;
+            age = AnimalAge;
+            weight = AnimalWeight;
+            _healthComp = new HealthComp(10);
+            _healthComp.onDie += Die;
+            _healthComp.onHealthChange += HealthUpdated;
+        }
+        void HealthUpdated(float newHealth)
+        {
+            Orange.PrintObject($"{name} : my health is now: {newHealth}");
+        }
+        void Die()
+        { 
+            Orange.PrintObject($"{name} : Ah! I am dead");
+        }
+
+        HealthComp _healthComp;
+        public HealthComp healthComp
+        {
+            get { return _healthComp; }
+        }
+        
+        string name;
+        public string Name
+        {
+            set
+            {
+                name = value;
+            }
+            get
+            {
+                return name;
+            }
+        }
+        int age;
+        public int Age
+        {
+            set { age = value; }
+            get { return age; }
+        }
+
+        float weight;
+        public float Weight
+        {
+            set {
+                    weight = System.Math.Clamp(value, 0, float.MaxValue);
+                }
+            get {
+                    return weight;
+                }
+        }
+
+        public void SetName(string newName)
+        {
+            name = newName;
+        }
+
+        //prefer to use setter and getter instead of public variables
+        public void SetAge(int newAge)
+        {
+            if(newAge < 0)
+            {
+                System.Exception newException = new System.Exception("age is smaller than 0");
+                throw newException;
+            }
+            newAge = System.Math.Clamp(newAge, 0, int.MaxValue);
+            age = newAge;
+        }
+        public int GetAge() { return age; }
+
+        public virtual string toString()
+        {
+            return $"{name} is {age} years old, weight {weight}";
+        }
+    }
+
+    class Dog : Animal
+    { 
+        public Dog() {}
+        public Dog(string AnimalName, int AnimalAge, float AnimalWeight, string favorateTreat)
+            : base(AnimalName, AnimalAge, AnimalWeight)
+        {
+            this.favorateTreat = favorateTreat;
+        }
+        string favorateTreat;
+        public override string toString()
+        {
+            return $"{Name} is a Dog, {Age} years old, weight: {Weight}, likes to eat: {favorateTreat}";
+        }
+    }
+
+    class Bird : Animal
+    {
+        public Bird() { }
+        public Bird(string AnimalName, int AnimalAge, float AnimalWeight, float flyHeight)
+            : base(AnimalName, AnimalAge, AnimalWeight)
+        {
+            this.flyHeight = flyHeight;
+        }
+        float flyHeight;
+        public override string toString()
+        {
+            return $"{Name} is a Bird, {Age} years old, weight: {Weight}, flies {flyHeight} hight";
+        }
+    }
+
+    class Car : IObjectGenericInfo
+    {
+        public float GetWeight() { return weight; }
+        public string GetName() { return $"{year} {make} {model}"; }
+        public Car() { }
+        public Car(float weight, string make, string model, int year)
+        {
+            this.weight = weight;
+            this.make = make;
+            this.model = model;
+            this.year = year;
+            _healthComp = new HealthComp(10);
+            _healthComp.onDie += Die;
+            _healthComp.onHealthChange += HealthUpdated;
+        }
+
+        void HealthUpdated(float newHealth)
+        {
+            Orange.PrintObject($"{GetName()} : my health is now: {newHealth}");
+        }
+        void Die()
+        {
+            Orange.PrintObject($"{GetName()} blews up!");
+        }
+
+
+        HealthComp _healthComp;
+        public HealthComp healthComp
+        {
+            get { return _healthComp; }
+        }
+
+        string make;
+        string model;
+        int year;
+        float weight;
+        public float Weight
+        {
+            set { weight = value; }
+            get { return weight; }
+        }
+    }
+
+    //public member is dangerous, fragile
+    class Date
+    {
+        int year;
+        int month;
+        int day;
+
+        public void IncrementDate(int daysToIncrement)
+        {
+            
+        }
+    }
+}
+
---
+++ CSharpDemo/Program.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using CSharpDemo;
+using System.Collections.Generic;
+enum PlayerState
+{
+    Idle,
+    Walk,
+    Die
+}
+
+class Orange
+{
+    //delegate
+    public delegate bool SortRule(int x, int y);
+    static SortRule AscendingRule = (x, y) => x > y;
+    static SortRule DecendingRule = (x, y) => x < y;
+    static SortRule ReverseRule = (x, y) => true;
+
+    //lambda
+    static Func<int, int, bool> AscendingFuc = (x, y) => x > y;
+
+    //Action
+    static Action<int, int> action = (x, y) => PrintObject("This is an action called");
+
+    static void Main()
+    {
+        action(1, 2);
+        //1 bit is a 0 or 1
+        //1 byte is 8 bit
+        //1 kb is 1024 kilobyte
+        //1 mb is 1024 kilobyte
+        //1 gb is 1024 megabyte
+
+        //an int is 4 byte
+        int intVar = (int)3.1f;
+
+        float floatVar = 1.00000015f;
+        char charVar = 'a';
+
+        //string is an array of characters
+        string stringVar = "name, sfsdis ,sdf s, sdfsdfsfsfvd sdfsdfsvsdf dmdfdf , sfdsdfsdfsdf";
+        //just like this int array
+        int[] array = { 1, 2, 3, 4 };
+
+        System.Console.WriteLine(stringVar[3]);
+
+        //short circuit
+        if (GetABool() || GetAnoterBool())
+        {
+
+        }
+
+        //while
+        int i = 0;
+        while (i < 10)
+        {
+            System.Console.WriteLine(i);
+            i++;
+        }
+        System.Console.WriteLine(i);
+        System.Console.WriteLine("=========================================================");
+
+        for (int j = 0/*happens in the begining only once*/; j < 10/*happens in the begining of the loop*/; j++/*happens at the end of the loop*/)
+        {
+            System.Console.WriteLine(j);
+
+        }
+
+        System.Console.WriteLine("=========================================================");
+        int k = 0;
+        for (; k < 10;)
+        {
+            System.Console.WriteLine(k);
+            k++;
+        }
+
+        do
+        {
+            System.Console.WriteLine("guarenteened to run at least once");
+        } while (false);
+
+        //using string is not only taking more memory, but also hard to track
+        string payerStates = "Idling";
+        //use a enum is cleaner
+        PlayerState playerState = PlayerState.Walk;
+
+        switch ((PlayerState)3)
+        {
+            case PlayerState.Idle:
+                System.Console.WriteLine("Player is idle");
+                break;
+            case PlayerState.Walk:
+                System.Console.WriteLine("Player is walking");
+                break;
+            case PlayerState.Die:
+                System.Console.WriteLine("Player is dead");
+                break;
+            default:
+                System.Console.WriteLine("Player is in a default state");
+                break;
+        }
+        System.Console.WriteLine(sizeof(PlayerState));
+
+        Dictionary<string, int> Employees = new Dictionary<string, int>();
+        Employees.Add("Bob", 20);
+        Employees.Add("Ugine", 21);
+
+        Employees.TryGetValue("Ugine", out int BobAge);
+        System.Console.WriteLine(BobAge);
+
+        System.Collections.Generic.Dictionary<string, int>.KeyCollection keys = Employees.Keys;
+        foreach (var key in keys)
+        {
+            System.Console.WriteLine(key);
+        }
+
+        var valus = Employees.Values;
+        foreach (var val in valus)
+        {
+            System.Console.WriteLine(val);
+        }
+
+        Dictionary<PlayerState, int> stateNumbers = new Dictionary<PlayerState, int>();
+        
+        PrintObject<string, int>("haha", 3);
+        PrintObject<int, float>(1, 1.2f);
+        PrintObject<PlayerState, bool>(playerState, false);
+
+        Animal animal = new Animal();
+
+        Animal animal2 = new Animal("Mocha", 7, 21);
+        PrintObject(animal2.GetAge());
+
+        animal2.Weight = 23;
+        PrintObject(animal2.Weight);
+
+        int valueToIncrement = 0;
+        IncrementInt(ref valueToIncrement);
+        PrintObject(valueToIncrement);
+
+        animal2.SetAge(2);
+        IncrmentAnimalAge(animal2);
+        PrintObject(animal2.GetAge());
+
+        Dog dog = new Dog("Spike", 6, 70, "bacon");
+        Bird bird = new Bird("Sparow", 4, 10, 200);
+        Car car = new Car(20, "Toyota", "RV4", 2021);
+        IObjectGenericInfo[] objs = { animal, animal2, dog, bird, car};
+
+        car.healthComp.TakeDamage(10);
+
+        foreach(IObjectGenericInfo animalItem in objs)
+        {
+            PrintObject(animalItem.GetName());
+        }
+
+        int[] intArryToSort = { 2, 1, 3, 5, 4 };
+
+
+        
+        Sort(intArryToSort, ReverseRule);
+        
+        //lambda 
+        Sort(intArryToSort, AscendingFuc);
+        
+        foreach (int arrayItem in intArryToSort)
+        {
+            PrintObject(arrayItem);
+        }
+    }
+
+
+    static void Sort(int[] arrayToSort, SortRule rule)
+    {
+        for (int i = 0; i < arrayToSort.Length; i++)
+        {
+            for (int j = i + 1; j < arrayToSort.Length; j++)
+            {
+                bool SholdSwitch = rule(arrayToSort[i], arrayToSort[j]);
+                if (SholdSwitch)
+                {
+                    int temp = arrayToSort[i];
+                    arrayToSort[i] = arrayToSort[j];
+                    arrayToSort[j] = temp;
+                }
+            }
+        }
+    }
+
+    static void Sort(int[] arrayToSort, Func<int, int, bool> rule)
+    {
+        for (int i = 0; i < arrayToSort.Length; i++)
+        {
+            for (int j = i + 1; j < arrayToSort.Length; j++)
+            {
+                bool SholdSwitch = rule(arrayToSort[i], arrayToSort[j]);
+                if (SholdSwitch)
+                {
+                    int temp = arrayToSort[i];
+                    arrayToSort[i] = arrayToSort[j];
+                    arrayToSort[j] = temp;
+                }
+            }
+        }
+    }
+    static void IncrmentAnimalAge(Animal animalToIncrement)
+    {
+        animalToIncrement.SetAge(animalToIncrement.GetAge() + 1);
+    }
+
+    static void IncrementInt(ref int intToIncrement)
+    {
+        intToIncrement++;
+    }
+
+    public static void PrintObject<T>(T ObjToPrint)
+    {
+        System.Console.WriteLine(ObjToPrint);
+    }
+    //generic programming, T, and T2 can be any type.
+    static void PrintObject<T, T2>(T objectToPrint, T2 OtherObjToPrint)
+    {
+        System.Console.WriteLine(objectToPrint);
+        System.Console.WriteLine(OtherObjToPrint);
+    }
+
+    static bool GetABool()
+    {
+        return true;
+    }
+
+    static bool GetAnoterBool()
+    {
+        System.Console.WriteLine("Getting another bool");
+        return true;
+    }
+}
